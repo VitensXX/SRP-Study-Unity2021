97c3779268c61aaaa76d68d80fa951eb0a508e48
Your Name <vitens@foxmail.com>
2022-10-18 22:27:49 +0800

    draw call

--- Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
+++ Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
@@ -3,6 +3,16 @@
 #define CUSTOM_UNITY_INPUT_INCLUDED
 
 float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+
 float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 glstate_matrix_projection;
+
+float4x4 unity_prev_MatrixM;
+float4x4 unity_prev_MatrixIM;
+
+//这个矩阵包含一些再这里我们不需要的转换信息
+real4 unity_WorldTransformParams;
 
 #endif
