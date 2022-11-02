138b99eba494798d4f28c8883afc0d4c657b79d3
VitensXX <vitens@foxmail.com>
2022-11-02 19:51:39 +0800

    srp batch

--- Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
+++ Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
@@ -2,8 +2,13 @@
 #ifndef CUSTOM_UNITY_INPUT_INCLUDED
 #define CUSTOM_UNITY_INPUT_INCLUDED
 
+CBUFFER_START(UnityPerDraw)
 float4x4 unity_ObjectToWorld;
 float4x4 unity_WorldToObject;
+float4 unity_LODFade;
+//这个矩阵包含一些再这里我们不需要的转换信息
+real4 unity_WorldTransformParams;
+CBUFFER_END
 
 float4x4 unity_MatrixVP;
 float4x4 unity_MatrixV;
@@ -12,7 +17,6 @@ float4x4 glstate_matrix_projection;
 float4x4 unity_prev_MatrixM;
 float4x4 unity_prev_MatrixIM;
 
-//这个矩阵包含一些再这里我们不需要的转换信息
-real4 unity_WorldTransformParams;
+
 
 #endif
