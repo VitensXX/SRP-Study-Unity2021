2d73b94c7a0e5dced60fa54eede076dbe67a4c12
VitensXX <vitens@foxmail.com>
2022-12-21 18:43:53 +0800

    shadow cascade

--- Assets/Custom RP/Runtime/Shadows.cs
+++ Assets/Custom RP/Runtime/Shadows.cs
@@ -3,11 +3,12 @@ using UnityEngine.Rendering;
 
 public class Shadows {
     const int maxShadowedDirectionalLightCount = 4;
+	const int maxCascades = 4;
 
 	const string bufferName = "Shadows";
     static int dirShadowAtlasId = Shader.PropertyToID("_DirectionalShadowAtlas");
 	static int dirShadowMatricesId = Shader.PropertyToID("_DirectionalShadowMatrices");
-	static Matrix4x4[] dirShadowMatrices = new Matrix4x4[maxShadowedDirectionalLightCount];
+	static Matrix4x4[] dirShadowMatrices = new Matrix4x4[maxShadowedDirectionalLightCount * maxCascades];
 
 	CommandBuffer buffer = new CommandBuffer {
 		name = bufferName
@@ -45,7 +46,7 @@ public class Shadows {
 				visibleLightIndex = visibleLightIndex
 			};
 
-			return new Vector2(light.shadowStrength, ShadowedDirectionalLightCount++);
+			return new Vector2(light.shadowStrength, settings.directional.cascadeCount * ShadowedDirectionalLightCount++);
 		}
 
 		return Vector2.zero;
@@ -66,7 +67,9 @@ public class Shadows {
         buffer.BeginSample(bufferName);
 		ExecuteBuffer();
 
-		int split = ShadowedDirectionalLightCount <= 1 ? 1 : 2;
+		int tiles = ShadowedDirectionalLightCount * settings.directional.cascadeCount;
+		int split = tiles <= 1 ? 1 : tiles <= 4 ? 2 : 4;
+		// int split = ShadowedDirectionalLightCount <= 1 ? 1 : 2;
 		int tileSize = atlasSize / split;
 
         for (int i = 0; i < ShadowedDirectionalLightCount; i++) {
@@ -81,23 +84,30 @@ public class Shadows {
     void RenderDirectionalShadows (int index, int split, int tileSize) {
         ShadowedDirectionalLight light = ShadowedDirectionalLights[index];
 		var shadowSettings = new ShadowDrawingSettings(cullingResults, light.visibleLightIndex);
-        //找到与光的方向匹配的视图和投影矩阵，并给一个裁剪空间的立方体，该立方体与包含光源阴影的摄像机的可见区域重叠
-        cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives(
-			light.visibleLightIndex, 0, 1, Vector3.zero, tileSize, 0f,
-			out Matrix4x4 viewMatrix, out Matrix4x4 projectionMatrix,
-			out ShadowSplitData splitData
-		);
-		shadowSettings.splitData = splitData;
-		//得到世界空间到灯光空间的转换矩阵
-		// dirShadowMatrices[index] = projectionMatrix * viewMatrix;
-		dirShadowMatrices[index] = ConvertToAtlasMatrix(
-			projectionMatrix * viewMatrix,
-			SetTileViewport(index, split, tileSize), split
-		);
-		// SetTileViewport(index, split, tileSize);
-		buffer.SetViewProjectionMatrices(viewMatrix, projectionMatrix);
-		ExecuteBuffer();
-		context.DrawShadows(ref shadowSettings);
+		int cascadeCount = settings.directional.cascadeCount;
+		int tileOffset = index * cascadeCount;
+		Vector3 ratios = settings.directional.CascadeRatios;
+
+		for (int i = 0; i < cascadeCount; i++) {
+			//找到与光的方向匹配的视图和投影矩阵，并给一个裁剪空间的立方体，该立方体与包含光源阴影的摄像机的可见区域重叠
+			cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives(
+				light.visibleLightIndex, i, cascadeCount, ratios, tileSize, 0f,
+				out Matrix4x4 viewMatrix, out Matrix4x4 projectionMatrix,
+				out ShadowSplitData splitData
+			);
+			shadowSettings.splitData = splitData;
+			int tileIndex = tileOffset + i;
+			//得到世界空间到灯光空间的转换矩阵
+			// dirShadowMatrices[index] = projectionMatrix * viewMatrix;
+			dirShadowMatrices[tileIndex] = ConvertToAtlasMatrix(
+				projectionMatrix * viewMatrix,
+				SetTileViewport(tileIndex, split, tileSize), split
+			);
+			// SetTileViewport(index, split, tileSize);
+			buffer.SetViewProjectionMatrices(viewMatrix, projectionMatrix);
+			ExecuteBuffer();
+			context.DrawShadows(ref shadowSettings);
+		}
     }
 
 	Vector2 SetTileViewport (int index, int split, float tileSize) {
