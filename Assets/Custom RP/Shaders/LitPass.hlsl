72a31a7057bc856b06099fd96478b76daa87b29d
VitensXX <vitens@foxmail.com>
2023-10-31 19:11:25 +0800

    lightmap

--- Assets/Custom RP/Shaders/LitPass.hlsl
+++ Assets/Custom RP/Shaders/LitPass.hlsl
@@ -6,6 +6,7 @@
 #include "../ShaderLibrary/Shadows.hlsl"
 #include "../ShaderLibrary/Light.hlsl"
 #include "../ShaderLibrary/BRDF.hlsl"
+#include "../ShaderLibrary/GI.hlsl"
 #include "../ShaderLibrary/Lighting.hlsl"
 
 // CBUFFER_START(UnityPerMaterial)
@@ -29,6 +30,7 @@ struct Attributes {
 	float3 positionOS : POSITION;
 	float3 normalOS : NORMAL;
 	float2 baseUV : TEXCOORD0;
+	GI_ATTRIBUTE_DATA
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -37,6 +39,7 @@ struct Varyings {
 	float3 positionWS : VAR_POSITION;
 	float3 normalWS : VAR_NORMAL;
 	float2 baseUV : VAR_BASE_UV;
+	GI_VARYINGS_DATA
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -46,6 +49,7 @@ Varyings LitPassVertex (Attributes input){
 	Varyings output;
 	UNITY_SETUP_INSTANCE_ID(input);
 	UNITY_TRANSFER_INSTANCE_ID(input, output);
+	TRANSFER_GI_DATA(input, output);
 	output.positionWS = TransformObjectToWorld(input.positionOS);
 	output.positionCS = TransformWorldToHClip(output.positionWS);
 	output.normalWS = TransformObjectToWorldNormal(input.normalOS);
@@ -85,8 +89,11 @@ float4 LitPassFragment (Varyings input) : SV_TARGET {
 		BRDF brdf = GetBRDF(surface, true);
 	#else
 		BRDF brdf = GetBRDF(surface);
-	#endif	
-	float3 color = GetLighting(surface, brdf);
+	#endif
+	
+	//获取全局光照
+	GI gi = GetGI(GI_FRAGMENT_DATA(input));
+	float3 color = GetLighting(surface, brdf, gi);
 	return float4(color, surface.alpha);
 }
 
